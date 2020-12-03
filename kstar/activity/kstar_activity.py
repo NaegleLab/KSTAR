ccc4e3a3b52fad1a71dc6d98b2c68745ebfc48c0
Kristen Naegle <kmn4mj@virginia.edu>
2020-12-03 18:32:58 -0500

    Fixed an issue in single core processing, default config to single core

--- kstar/activity/kstar_activity.py
+++ kstar/activity/kstar_activity.py
@@ -313,7 +313,7 @@ class KinaseActivity:
         # if no data columns are provided use all columns that start with data:
         # data columns that filtered have no evidence are removed
         self.set_data_columns(data_columns)
-        self.logger.info(f"Kinase Activity run on following data columns : {','.join(self.data_columns)}")
+        self.logger.info(f"Kinase Activity will be run on the following data columns: {','.join(self.data_columns)}")
         
 
         evidence = self.evidence.groupby([config.KSTAR_ACCESSION, config.KSTAR_SITE]).agg(agg).reset_index()
@@ -336,7 +336,7 @@ class KinaseActivity:
                     filtered_evidence = evidence[evidence[col] > threshold]
                 else:
                     filtered_evidence = evidence[evidence[col] < threshold]
-                act = self.calculate_hypergeometric_activities(filtered_evidence)
+                act = self.calculate_hypergeometric_activities(filtered_evidence, col)
                 act['data'] = col
                 activities_list.append(act)
         self.activities = pd.concat(activities_list)
--- kstar/config.py
+++ kstar/config.py
@@ -39,8 +39,8 @@ KSTAR_PEPTIDE = 'KSTAR_PEPTIDE'
 KSTAR_SITE = 'KSTAR_SITE'
 KSTAR_KINASE = 'Kinase Name'
 
-# Number of cores to use for parallelization
-PROCESSES = 4
+# Number of cores to use for parallelization, set to 1 to avoid multiprocessing
+PROCESSES = 1
 
 ## END DECLARATION OF GLOBALS
 
