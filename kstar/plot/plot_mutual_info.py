d97492c7caf998e2cd8424d7cb873e30d58b970d
Kristen Naegle <kmn4mj@virginia.edu>
2021-04-23 16:24:28 -0400

    Added ability to calculate average MI across kinase networks and optionally on a subset of substrates, such as evidence used in an experiment

--- kstar/plot/plot_mutual_info.py
+++ kstar/plot/plot_mutual_info.py
@@ -81,7 +81,8 @@ def kinase_network_similarity(network1, network2, kinase_column, substrate_colum
     return heatmap, normalized, heatlist
     
 
-def kinase_mutual_information(network, kinase_column, substrate_column):
+
+def kinase_mutual_information(network, kinase_column, substrate_column, substrate_list=[]):
     """
     Finds mutual information shared between kinases based on the substrate phosphorylated
     Mutual Information is defined as the intersection substrates between two kinases
@@ -91,12 +92,82 @@ def kinase_mutual_information(network, kinase_column, substrate_column):
 
     Parameters
     ----------
-    network : pandas dataframe
-        The network to analyze for mutual kinase information
+    network : pandas dataframe or dictionary of pandas dataframe
+        The network to analyze for mutual kinase information. Can send a dictionary of multiple pandas dataframes and this will average the MI across all networks in dictionary
     kinase_column : str
         Column in network that contiains kinase information
     substrate_column : str
         Column in network that contains substrate information
+    substrate_list : list
+        Optional and default is no subset list to use. You can calculate the MI within network(s) for only the evidence given in a substrate_evidence_list (must matche substrate_column of network passed in)
+    
+    Returns
+    --------
+    heatmap : pandas dataframe
+        Number of substrates that overlap between kinases
+    normalized : pandas dataframe
+        Normalized mutual information into Jaccard Index. 
+        size of intersection of two kinase networks / size of union of two kinase networks.
+    heatlist or heatdict: list or dictionary of lists
+        intersection of kinase networks. If a single network it is a list. If multiple networks it is a dict of lists with keys the same as the network name
+
+    """
+    AVG = 0
+    if isinstance(network, dict):
+        heatlist = {}
+        AVG = 1
+        networkNames = list(network.keys())
+        #get a network to start and then march through the rest, averaging
+        net = network[networkNames[0]]
+        if substrate_list:
+            net = net[net[substrate_column].isin(substrate_list)]
+            if net.empty:
+                raise ValueError("Mapping of substrate_list to the network failed")
+                return -1, -1, -1
+        heatmap_all, normalized_all, heatlist[networkNames[0]] = kinase_mutual_information_singleNetwork(net, kinase_column, substrate_column)
+        for i in range(1,len(networkNames)):
+            net = network[networkNames[i]]
+            if substrate_list:
+                net = net[net[substrate_column].isin(substrate_list)]
+                if net.empty:
+                    raise ValueError("Mapping of substrate_list to the network failed")
+                    return -1, -1, -1
+            heatmap, normalized, heatlist[networkNames[i]] = kinase_mutual_information_singleNetwork(net, kinase_column, substrate_column)
+            normalized_all = pd.concat([normalized_all, normalized], sort=True)
+            heatmap_all = pd.concat([heatmap_all, heatmap], sort=True)
+        average_MI = normalized_all.groupby(level=0).mean()
+        average_heatmap = heatmap_all.groupby(level=0).mean()
+        return average_heatmap, average_MI, heatlist
+    else:
+        net = network
+        if substrate_list:
+            net = net[net[substrate_column].isin(substrate_list)]
+            if net.empty:
+                raise ValueError("Mapping of substrate_list to the network failed")
+                return -1, -1, -1
+        heatmap, normalized, heatlist = kinase_mutual_information_singleNetwork(net, kinase_column, substrate_column)
+        return heatmap, normalized, heatlist
+
+
+
+def kinase_mutual_information_singleNetwork(network, kinase_column, substrate_column):
+    """
+    Finds mutual information shared between kinases based on the substrate phosphorylated
+    Mutual Information is defined as the intersection substrates between two kinases
+    A substrate is defined as the substrate accession and site, i.e. P54760_Y596.
+    Normalization is performed by comparing intersection of kinases vs union of the two kinases
+    This the the Jaccard Index. Jaccard Distance can be calcualted by taking 1 - JI
+
+    Parameters
+    ----------
+    network : pandas dataframe or dictionary of pandas dataframe
+        The network to analyze for mutual kinase information. Can send a dictionary of multiple pandas dataframes and this will average the MI across tehm
+    kinase_column : str
+        Column in network that contiains kinase information
+    substrate_column : str
+        Column in network that contains substrate information
+    substrate_list : array
+        Optional and default is no subset list to use. You can calculate the MI within network(s) for only the evidence given in a substrate_evidence_list (must matche substrate_column of network passed in)
     
     Returns
     --------
@@ -106,7 +177,7 @@ def kinase_mutual_information(network, kinase_column, substrate_column):
         Normalized mutual information into Jaccard Index. 
         size of intersection of two kinase networks / size of union of two kinase networks.
     heatlist : pandas dataframe
-        intersction of kinase networks
+        intersection of kinase networks
     """
     kinases = list(network[kinase_column].unique())
     num_kinases = len(kinases)
