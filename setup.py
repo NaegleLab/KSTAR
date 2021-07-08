8cdcd2c983ea11009b60852cc27948e95c7235ee
Sam Crowl <crowl_samuel@ku.edu>
2021-07-08 10:40:51 -0400

    Updated setup for PyPI posting

--- setup.py
+++ setup.py
@@ -1,11 +1,27 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='kstar',
-      version='0.1',
-      description='KSTAR',
-      url='https://github.com/NaegleLab/KSTAR',
-      author='Naegle Lab',
-      author_email='kmn4mj@virginia.edu',
-      license='MIT',
-      packages=['kstar'],
-      zip_safe=False)
+
+# Run setup
+setup(
+    name="kstar",
+    version="0.1",
+    author="Naegle Lab",
+    author_email="kmn4mj@virginia.edu",
+    url="https://github.com/NaegleLab/KSTAR",
+    install_requires=['pandas', 'numpy', 'scipy', 'matplotlib', 'seaborn', 'statsmodels', 'biopython'],
+    license='GNU General Public License v3',
+    description="""KSTAR: Kinase-Substrate Transfer to Activity Relationships',
+    long_description="KSTAR is an open-source software for estimating kinase activities from phosphoproteomic data. 
+    KSTAR implements statistical and graph-theoretic approaches to produce a robust activity score that increases with 
+    increasing evidence from a kinase's network.""",
+    project_urls = {'Documentation': 'https://naeglelab.github.io/KSTAR/'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    python_requires=">=3.6",
+    zip_safe = False
+)
+
