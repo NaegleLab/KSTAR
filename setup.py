da777f0f0220af8be9272dd2dc3104d1dca24c01
Sam Crowl <crowl_samuel@ku.edu>
2021-07-16 17:19:13 -0400

    updated setup.py and meta.yaml for most recent installation

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 # Run setup
 setup(
     name="kstar",
-    version="0.2",
+    version="0.2.1",
     author="Naegle Lab",
     author_email="kmn4mj@virginia.edu",
     url="https://github.com/NaegleLab/KSTAR",
