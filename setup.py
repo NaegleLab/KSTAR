7a428a184de38727e6a5a6d0acd49eb722a20dfb
Sam Crowl <crowl_samuel@ku.edu>
2022-01-18 12:15:22 -0500

    Updates to packaging files

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 # Run setup
 setup(
     name="kstar",
-    version="0.3.1",
+    version="0.3.2",
     author="Naegle Lab",
     author_email="kmn4mj@virginia.edu",
     url="https://github.com/NaegleLab/KSTAR",
